210c6504453c26b1488a59f909fc10a7a6d4ad68
RileySuomi <suomir@spu.edu>
2025-05-13 19:41:10 -0700

    feat: added abstract

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,19 @@ function App() {
   return (
     <>
       <div>
+         <div className="space-y-2 text-center py-10 rounded-lg shadow-md">
+
+            <h2 className="text-2xl font-bold">Abstract</h2>
+            <div>
+                <p className="text-left mx-6 max-w-3xl text-gray-700">
+                    This is the Abstract where we will insert the text of the abstract. 
+                    I'm still typing so I see how it looks as far as UI for the webpage!
+                    So, this obviously isn't the final text, but it is a placeholder for now.
+                    I will be adding more text to this section to see how it looks. Okay,
+                    I think this is enough text for now.
+                </p>
+            </div>
+        </div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
